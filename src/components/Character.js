c86624fb1b1fd9d5ecf7e9b494624ddc02ab5de7
GenHero <51133244+GenHero@users.noreply.github.com>
2021-03-05 19:52:30 -0500

    test

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Accordion from 'react-bootstrap/Accordion'
+
+const Character = (props) =>{
+    const { characters } = props;
+
+
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
+
+
+const Container = styled.div`
+
+`
+
+export default Charcter;
